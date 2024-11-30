dca14e4c992260ce6cae99f22475d7a4ca1e2450
Theophilus Adebanjo <banjotheo4@gmail.com>
2024-11-30 18:38:40 +0100

    Create variables.py
    completed new module assignment

---
+++ assignments on modules/variables.py
@@ -0,0 +1,41 @@
+# Dat 2: 30 days of python programming
+# Exercise: Level 1
+first_name= 'Asani'
+last_name= 'Jamal'
+full_name= first_name + last_name
+country= 'Nigeria'
+city= 'Lagos'
+age= '23'
+year='2024'
+is_married= False
+is_true= True
+is_light_on= True
+school, faculty,department, level= 'Unilag','Social_science','Psychology',400
+# Exercise: Level 2
+print(type(first_name),type(last_name),type(full_name),type(country),type(city),
+            type(age),type(year),type(is_married),type(is_true),type(is_light_on),type(school),type(faculty),type(department),type(level))
+print(len(first_name))
+print(len(last_name))
+num_one=5
+num_two=4
+total= num_one+num_two
+diff= num_one-num_two
+product= num_two * num_one
+division= num_one/num_two
+remainder= num_two%num_one
+exp=num_one**num_two
+floor_division=num_one//num_two
+PI= 3.14
+r= 30
+area_of_circle= PI*r**2
+print(area_of_circle)
+circum_of_circle= 2*PI*30
+radius= float(input('radius: '))
+area_of_circle= PI*radius**2
+print('Aread of circle:', area_of_circle)
+first_name= input('first name? ')
+last_name= input('last name? ')
+country= input(' country? ')
+age= input('age? ')
+print(first_name,last_name,country,age)
+print(help('keywords'))
