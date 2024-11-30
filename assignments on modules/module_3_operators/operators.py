2f0338d24a8726badb5d01ade86dee3ca369cbc0
Theophilus Adebanjo <banjotheo4@gmail.com>
2024-11-30 19:28:47 +0100

    Update operators.py

---
+++ assignments on modules/module_3_operators/operators.py
@@ -0,0 +1,16 @@
+age= 24
+height=196.1
+yo= 1j + 1
+def cal_triangle_area():
+    b= float(input('Enter base: '))
+    h= float(input('Enter height: '))
+    area = (0.5 * b * h)
+    print("The area of the triangle is: ", area)
+cal_triangle_area()
+def per_of_triangle():
+    a=int(input("Enter side a: "))
+    b=int(input("Enter side b: "))
+    c= int(input("Enter side c: "))
+    perimeter= a+b+c
+    print("The perimeter of the triangle is", perimeter)
+per_of_triangle()
