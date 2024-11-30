214ad7b5f32180076f2d39e8f8be729966f78c34
Theophilus Adebanjo <banjotheo4@gmail.com>
2024-11-30 22:01:52 +0100

    Update strings.py
    part of my module 4 submission

---
+++ assignments on modules/module_4_strings/strings.py
@@ -0,0 +1,21 @@
+words= 'Thirty','Days','Of','Python'
+concat_words= ' '.join(words)
+print(concat_words)
+cil= 'Coding','For','All'
+concat_cil= ' '.join(cil)
+company= 'Coding For All'
+print(company)
+print(len(company))
+upp_company= company.upper()
+print(upp_company)
+low_company= company.lower()
+print('Coding For All'.capitalize())
+print('Coding For All'.title())
+print('Coding For All'.swapcase())
+ca= 'Coding For All'
+print(ca[1:])
+print(company.index('Coding'))
+print(company.replace('Coding','Python'))
+xt= 'Python For All'
+print(xt.replace('For','for'))
+print(xt.replace('All','Everyone'))  # 12
