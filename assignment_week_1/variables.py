99af2af77bb4c48db9d237ae3b8d94622518ba51
Theophilus Adebanjo <banjotheo4@gmail.com>
2024-11-30 10:19:09 +0100

    Create variables.py
    added solutions to parts 1 and 2 of the assignment

---
+++ assignment_week_1/variables.py
@@ -0,0 +1,10 @@
+# 1. Simple Variable Assignment
+x=10
+print(x)
+# 2. Assigning Different Data Types
+age=25
+height=5.9
+name= 'John'
+is_active= True
+print([age,height,name,is_active])
+# 3. Simple Addition
