ec5d57ee6a54b413b1fe45fcd8d5dfffa80b92f3
Theophilus Adebanjo <banjotheo4@gmail.com>
2024-11-30 10:43:24 +0100

    Update variables.py

--- assignment_week_1/variables.py
+++ assignment_week_1/variables.py
@@ -8,3 +8,21 @@ name= 'John'
 is_active= True
 print([age,height,name,is_active])
 # 3. Simple Addition
+a= 9
+b= 3
+print(a+b)
+# 4. String Concatenation
+first_name= 'Alice'
+last_name=' Wonderland'
+print(first_name+last_name)
+# 5. Variable Type Check
+x= 'Hello'
+print(type(x))
+# 6. Swap Two Variables
+a=10
+b=5
+a=a+b
+b=a-b
+a=a-b
+print([a,b])
+# 7. Arithmetic Operations
