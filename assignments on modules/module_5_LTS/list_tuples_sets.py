9229533e801e905ec0d55854ed3fe196c6397b85
Theophilus Adebanjo <banjotheo4@gmail.com>
2024-12-06 13:26:03 +0100

    Create list_tuples_sets.py

---
+++ assignments on modules/module_5_LTS/list_tuples_sets.py
@@ -0,0 +1,11 @@
+# Exercises: Level 1
+emp= []
+tem= [1,2,3,4,5,6]
+print(len(tem))
+print(tem[0])
+print(tem[2:-2])
+print(tem[-1])
+mixed_data_types= ['jamal',24,195,'single','2 bony lagos']
+it_companies= ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+print(it_companies)
+print(len(it_companies))
