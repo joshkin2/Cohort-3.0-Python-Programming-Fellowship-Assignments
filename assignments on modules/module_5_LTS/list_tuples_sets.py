60939466a579593b07df50de8b0e984eaec4b94b
Theophilus Adebanjo <banjotheo4@gmail.com>
2024-12-07 10:22:17 +0100

    Update list_tuples_sets.py
    done with level 1.

--- assignments on modules/module_5_LTS/list_tuples_sets.py
+++ assignments on modules/module_5_LTS/list_tuples_sets.py
@@ -12,3 +12,40 @@ print(len(it_companies))
 print(it_companies[0])
 print(it_companies[3])
 print(it_companies[-1])
+it_companies[1]= 'Tesla'
+print(it_companies)
+it_companies.append('Silicon')
+it_companies.insert(4,'Hugo')
+print(it_companies)
+it_companies[1]='TESLA'
+STR_S= '#; '
+SKK= list(STR_S)
+add_both= it_companies + SKK
+print(add_both)
+does_exist= 'Hugo' in it_companies
+print(does_exist)
+it_companies.sort()
+print(it_companies)
+it_companies.reverse()
+print(it_companies)
+print(it_companies[0:3])
+print(it_companies[-4:-1])
+print(it_companies[4])
+it_companies.remove('TESLA')
+print(it_companies)
+del it_companies[3:5]
+print(it_companies)
+del it_companies[-1]
+print(it_companies)
+del it_companies[0:]
+print(it_companies)
+del it_companies
+front_end=['HTML', 'CSS', 'JS','React','Redux']
+back_end=['Node','Express','MongoDB']
+full_stack= front_end + back_end
+print(full_stack)
+full_stack.insert(5, 'Python')
+full_stack.insert(6, 'SQL')
+print(full_stack)
+
+#Exercises: Level 2
