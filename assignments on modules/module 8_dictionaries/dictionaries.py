bf19ce160ed2851fcbc258eb3cae2de9fa855a44
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2025-01-06 16:10:19 +0100

    Update dictionaries.py
    dictionary assignment

---
+++ assignments on modules/module 8_dictionaries/dictionaries.py
@@ -0,0 +1,14 @@
+dog={}
+dog= {'name':'trey', 'color':'beige','breed':'maltese','legs':4,'age':5}
+student_dict={
+  'first_name':'leo',
+  'last_name':'dan',
+  'gender':'female',
+  'age':14,
+  'marital status':'single',
+  'skills':'coding',
+  'country':'iran',
+  'city':'izbeh',
+  'address':'2 oslo dr'
+  print(len(student_dict))
+  
